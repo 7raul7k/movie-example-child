b85af3908beccd94aa44788b9046624ba1c5f33b
7raul7k <balanescuraul@gmail.com>
2023-09-11 21:13:15 +0300

    add home-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/home/header/header.component';
 import {HttpClientModule} from "@angular/common/http";
-import { AddMovieComponent } from './components/add-movie/add-movie.component';
-import { MovieComponent } from './components/movie/movie.component';
+import { AddMovieComponent } from './components/home/add-movie/add-movie.component';
+import { MovieComponent } from './components/home/movie/movie.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AddMovieComponent,
-    MovieComponent
+    MovieComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
